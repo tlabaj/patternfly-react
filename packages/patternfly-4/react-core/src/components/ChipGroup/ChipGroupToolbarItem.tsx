258c7579d90541b7a9536e90c600aebbe4bd4a53
Titani <tlabaj@redaht.com>
2020-02-14 09:28:28 -0500

    start adding tooltip to label

--- packages/patternfly-4/react-core/src/components/ChipGroup/ChipGroupToolbarItem.tsx
+++ packages/patternfly-4/react-core/src/components/ChipGroup/ChipGroupToolbarItem.tsx
@@ -3,12 +3,15 @@ import { css } from '@patternfly/react-styles';
 import styles from '@patternfly/react-styles/css/components/ChipGroup/chip-group';
 import { ChipGroupContext } from './ChipGroup';
 import { ChipButton } from './ChipButton';
+import { Tooltip } from '../Tooltip';
 import TimesIcon from '@patternfly/react-icons/dist/js/icons/times-icon';
 import GenerateId from '../../helpers/GenerateId/GenerateId';
 
 export interface ChipGroupToolbarItemProps extends React.HTMLProps<HTMLUListElement> {
   /**  Category name text */
   categoryName?: string;
+  /** Position of the tooltip which is displayed if the category name text is longer */
+  tooltipPosition?: 'auto' | 'top' | 'bottom' | 'left' | 'right';
   /** Content rendered inside the chip text */
   children: React.ReactNode;
   /** Additional classes added to the chip item */
@@ -21,45 +24,69 @@ export interface ChipGroupToolbarItemProps extends React.HTMLProps<HTMLUListElem
   closeBtnAriaLabel?: string;
 }
 
-export const ChipGroupToolbarItem: React.FunctionComponent<ChipGroupToolbarItemProps> = ({
-  categoryName = '',
-  children = null,
-  className = '',
-  isClosable = false,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  onClick = (_e: React.MouseEvent) => undefined as any,
-  closeBtnAriaLabel = 'Close chip group',
-  ...props
-}: ChipGroupToolbarItemProps) => {
-  if (React.Children.count(children)) {
-    const renderChipGroup = (id: string, HeadingLevel: any) => (
-      <ul className={css(styles.chipGroup, styles.modifiers.toolbar, className)} {...props}>
-        <li>
-          <HeadingLevel className={css(styles.chipGroupLabel)} id={id}>
-            {categoryName}
-          </HeadingLevel>
-          <ul className={css(styles.chipGroup)}>{children}</ul>
-          {isClosable && (
-            <div className="pf-c-chip-group__close">
-              <ChipButton
-                aria-label={closeBtnAriaLabel}
-                onClick={onClick}
-                id={`remove_group_${id}`}
-                aria-labelledby={`remove_group_${id} ${id}`}
-              >
-                <TimesIcon aria-hidden="true" />
-              </ChipButton>
-            </div>
-          )}
-        </li>
-      </ul>
-    );
+interface ChipGroupToolbarItemState {
+  isTooltipVisible: boolean;
+}
 
-    return (
-      <ChipGroupContext.Consumer>
-        {(HeadingLevel: any) => <GenerateId>{randomId => renderChipGroup(randomId, HeadingLevel)}</GenerateId>}
-      </ChipGroupContext.Consumer>
-    );
+export class ChipGroupToolbarItem extends React.Component<ChipGroupToolbarItemProps, ChipGroupToolbarItemState> {
+  constructor(props: ChipGroupToolbarItemProps) {
+    super(props);
+    this.state = {
+      isTooltipVisible: false
+    };
   }
-  return null;
-};
+
+  span = React.createRef<HTMLSpanElement>();
+
+  static defaultProps: ChipGroupToolbarItemProps = {
+    categoryName:  '',
+    children: null,
+    className: '',
+    isClosable: false,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    onClick: (_e: React.MouseEvent) => undefined as any,
+    closeBtnAriaLabel: 'Close chip group',
+    tooltipPosition: 'top' as 'auto' | 'top' | 'bottom' | 'left' | 'right'
+  }
+
+  // componentDidMount() {
+  //   this.setState({
+  //     isTooltipVisible: Boolean(this.span.current && this.span.current.offsetWidth < this.span.current.scrollWidth)
+  //   });
+  // }
+
+  render() {
+    const { categoryName, children, className, isClosable, closeBtnAriaLabel, onClick, tooltipPosition } = this.props;
+    if (React.Children.count(children)) {
+      const renderChipGroup = (id: string, HeadingLevel: any) => (
+        <ul className={css(styles.chipGroup, styles.modifiers.toolbar, className)} {...this.props}>
+          <li>
+            <HeadingLevel className={css(styles.chipGroupLabel)} id={id}>
+              {categoryName}
+            </HeadingLevel>
+            <ul className={css(styles.chipGroup)}>{children}</ul>
+            {isClosable && (
+              <div className="pf-c-chip-group__close">
+                <ChipButton
+                  aria-label={closeBtnAriaLabel}
+                  onClick={onClick}
+                  id={`remove_group_${id}`}
+                  aria-labelledby={`remove_group_${id} ${id}`}
+                >
+                  <TimesIcon aria-hidden="true" />
+                </ChipButton>
+              </div>
+            )}
+          </li>
+        </ul>
+      );
+
+      return (
+        <ChipGroupContext.Consumer>
+          {(HeadingLevel: any) => <GenerateId>{randomId => renderChipGroup(randomId, HeadingLevel)}</GenerateId>}
+        </ChipGroupContext.Consumer>
+      );
+    }
+    return null;
+  }
+}
